0702db2a9122e443931e140e8d73ee9919ff5475
Peksa <git@peksa.se>
2023-08-07 16:10:40 +0200

    Initial commit

---
+++ src/main/java/net/peksa/sba/broken/BrokenApplication.java
@@ -0,0 +1,16 @@
+package net.peksa.sba.broken;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class BrokenApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BrokenApplication.class, args);
+	}
+}
